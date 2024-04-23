b26ec3882ded9e60775a452e0584a1524543d399
Brian Kesner <brk828@gmail.com>
2024-04-23 15:51:06 -0700

    Updated body depth

--- Processing_code/XYTE_body_depth.R
+++ Processing_code/XYTE_body_depth.R
@@ -5,6 +5,9 @@
 # Razorback Hump - Portz, D.E. and H.M. Tyus. 2004. 
 # Fish humps in two Colorado River fishes: a morphological response to cyprinid predation? 
 # Environmental Biology of Fishes 71:233-245.
+library(stats)
+library(dplyr)
+library(ggplot2)
 
 tl_bd_2004 <- function(TL){
   BodyDepth <- 0.196*TL + 2.517
@@ -17,12 +20,33 @@ XYTEBodyDepth <-
   read.csv("Processing_code/data/RazorbackSuckerBodyDepth.csv")
 
 TLBD_model <- BD_mm ~ TL_mm
+TLBD_model_transformed <- log(BD_mm) ~ TL_mm
 TLBD_results <- glm(TLBD_model, data = XYTEBodyDepth)
+TLBD_t_results <- glm(TLBD_model_transformed, data = XYTEBodyDepth)
 
-XYTEBodyDepth$Residuals <- resid(TLBD_results, type = "pearson")
+XYTEBodyDepth$Residuals <- residuals(TLBD_results)
+XYTEBodyDepth$TResiduals <- residuals(TLBD_t_results)
+
+# Shapiro-Wilk test for normality
+shapiro.test(XYTEBodyDepth$Residuals)
+shapiro.test(XYTEBodyDepth$TResiduals)
+
+qqnorm(XYTEBodyDepth$Residuals)
+qqline(XYTEBodyDepth$Residuals)
+
+qqnorm(XYTEBodyDepth$TResiduals)
+qqline(XYTEBodyDepth$TResiduals)
 
 Intercept2024 <- coef(TLBD_results)[1] %>% as.double()
 Slope2024 <- coef(TLBD_results)[2] %>% as.double()
+
+Intercept2024T <- coef(TLBD_t_results)[1] %>% as.double()
+Slope2024T <- coef(TLBD_t_results)[2] %>% as.double()
+
+tl_bd_2024T <- function(TL){
+  BodyDepth <- Slope2024T*TL + Intercept2024T
+  return((BodyDepth)^exp())
+}
 
 tl_bd_2024 <- function(TL){
   BodyDepth <- Slope2024*TL + Intercept2024
@@ -31,11 +55,20 @@ tl_bd_2024 <- function(TL){
 
 BD_plot <- ggplot(XYTEBodyDepth, aes(x = TL_mm, y = BD_mm, color = Source)) +
   geom_point() +
-  stat_function(fun = tl_bd_2004, color = "blue", size = 1) + 
-  stat_function(fun = tl_bd_2024, color = "red", size = 1) 
+  stat_function(fun = tl_bd_2004, color = "black") + 
+  stat_function(fun = tl_bd_2024T, color = "red") +
+  stat_function(fun = tl_bd_2024, color = "white") 
 
-BD_residuals_plot <- ggplot(XYTEBodyDepth, aes(x = TL_mm, y = Residuals, color = Source)) +
+BD_residuals_plot <- ggplot(XYTEBodyDepth, aes(x = TL_mm, y = TResiduals, color = Source)) +
   geom_point() + 
-  geom_smooth(method="lm", se=FALSE, color="black")
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+  labs(x = "Total length (mm)", y = "Residuals", title = "Residual Plot")
+
 BD_plot
 
+BD_residuals_plot
+
+ggplot(your_data, aes(x = x, y = residuals)) +
+  geom_point() +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+  labs(x = "Predictor (x)", y = "Residuals", title = "Residual Plot")

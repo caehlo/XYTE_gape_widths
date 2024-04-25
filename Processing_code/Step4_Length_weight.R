a3016056dceebf1b2736f37232d529784bad6229
Brian Kesner <brk828@gmail.com>
2024-04-25 08:27:31 -0700

    Length-weight changes

--- Processing_code/Step4_Length_weight.R
+++ Processing_code/Step4_Length_weight.R
@@ -5,6 +5,15 @@ flathead <- read_csv("raw_data/flathead.csv") %>%
   mutate(Date = as.Date(Date, "%m/%d/%Y")) %>%
   mutate(PredMass = .000006*(TL^3.0997)) #length-weight relationship from Granfors (2014)
 
+# USGS data set Mississippi river data 1983 - 2023 
+# https://www.umesc.usgs.gov/cgi-bin/ltrmp/fish/fish_query.pl
+lt_flathead <- read.csv("raw_data/ltrm_fish_data_flathead_lw.csv") %>%
+  rename(TL = length) %>%
+  mutate(Species = "PYOL", Mass = as.integer(weight),
+         Date = as.Date(sdate,  "%m/%d/%Y"), 
+         Reach = NA, Agency = "USGS") %>%
+  select(Date, Species, TL, Mass, Reach, Agency)
+
 others <- read_excel("raw_data/Fish.xlsx") %>% mutate(Date = as.Date(substr(Date,1,6), "%y%m%d")) %>%
   mutate(Species = case_when(Species == 'Largemouth Bass' ~ 'MISA',
                              Species == 'Smallmouth Bass' ~ 'MIDO',
@@ -13,20 +22,22 @@ others <- read_excel("raw_data/Fish.xlsx") %>% mutate(Date = as.Date(substr(Date
   rename('TL' = 'Total Length', 'Mass' = 'Weight') %>%
   filter(Reach %in% c('2', '3'), !(Species == 'MISA' & TL > 650))
 
+nonnatives <- rbind(lt_flathead, others)
 
-species = c('MIDO', 'MOSA', 'MISA')
+species = c('MIDO', 'MOSA', 'MISA', 'PYOL')
 dataList = list()
 
 for(i in species){
-  temp <- others %>% filter(Species == i)
+  temp <- nonnatives %>% filter(Species == i)
   model <- glm(Mass ~ TL, temp, family = poisson(link = "log"))
   temp$PredMass <- predict(model, newdata = temp, type = 'response')
-  dataList[[i]] <- temp
+  dataList[[i]] <- list("data" = temp, "parameters" = coef(model))
 }
-withMass <- bind_rows(dataList)
+
+withMass <- bind_rows(dataList$data)
 
 
-nonnatives <- bind_rows(withMass, flathead)
+# nonnatives <- bind_rows(withMass, flathead)
 saveRDS(nonnatives, file = 'nonnatives.rds')
-ggplot(nonnatives, aes(x = TL, color = Species)) + geom_point(aes(y = Mass)) + geom_line(aes(y = PredMass)) +
+ggplot(withMass, aes(x = TL, color = Species)) + geom_point(aes(y = Mass)) + geom_line(aes(y = PredMass)) +
   facet_wrap(~Species, scales = 'free')
